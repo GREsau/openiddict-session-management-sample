5603c1fc6db7cadf08c7f206c0f4dd2c53650c82
Graham Esau <graham.esau@vonage.com>
2024-06-10 11:02:57 +0100

    Add Contruum sample, copied (with minor adjustments) from https://github.com/openiddict/openiddict-samples/tree/dev/samples/Contruum/Contruum.Server

---
+++ Contruum/Contruum.Server/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contruum.Server.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+}
